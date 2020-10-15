d66a566c1f26526d5e63512ca2fad57266660352
Sam Mathew <51379693+dayumsam@users.noreply.github.com>
2020-10-15 17:02:01 +0530

    Added the delete button

--- calc.js
+++ calc.js
@@ -1,12 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
     var numButtons = document.querySelectorAll(".num");
-    var value = document.querySelector("#value")
+    var value = document.querySelector("#value");
+    var del = document.querySelector("#delete")
 
     for(var i=0; i<numButtons.length; i++){
         numButtons[i].addEventListener("click", function(){
             value.innerHTML += this.innerHTML
-            console.log(value.innerHTML.length)
         })
     }
+
+    del.addEventListener("click", function(){
+        var delval =Math.floor(value.innerHTML/10) 
+        value.innerHTML = delval
+    })
 
 });
